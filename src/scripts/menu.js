ac8dda55446cb8d7d1cc6c25b64518f17ff2d293
Milosz <milwrona@gmail.com>
2025-04-14 16:59:43 +0200

    some changes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
+  const hamburger = document.querySelector(".hamburger");
+  hamburger.classList.toggle("expanded");
   document.querySelector(".nav-links").classList.toggle("expanded");
 });
